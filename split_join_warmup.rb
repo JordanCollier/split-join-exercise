642ba97c61e3b2225f13f8e232bb5d9b8152f772
Jordan Collier <jordanicollier@gmail.com>
2015-02-19 09:40:38 -0700

    5/7

--- split_join_warmup.rb
+++ split_join_warmup.rb
@@ -10,40 +10,46 @@ world_cup_news
 
 # enter your solutions inside the methods
 def goal(goal_message)
-
+  "#{goal_message}#{goal_message}"
 end
 
 def generate_an_array_of_teams(team_list)
-
+  arr = []
+  team_list.split(', ').each {|x| arr << x}
 end
 
 def number_of_teams(team_list)
-
+  team_list.split(', ').count
 end
 
 def return_australia(team_list)
-
+  arr = team_list.split(', ').to_s
+  arr["Australia"]
 end
 
 def starts_with_C(team_list)
+  emp = []
+  arr = team_list.split(', ')
+  arr.map {|x| if x.start_with?('C') ; emp << x end }
+  emp
 
 end
 
 def block_string_to_single_line(long_string)
-
+    long_string.split(",").join("").gsub("\n", " ")
 end
 
-def capitalize_every_third_word(long_string)
+  def capitalize_every_third_word(long_string)
 
-end
+  end
 
-check("goal method", goal(goal_message) == "GOOOOOOOAL!!!!GOOOOOOOAL!!!!")
-check("generate_an_array_of_teams method",
-generate_an_array_of_teams(team_list) == ["Brazil", "Mexico", "Cameroon", "Croatia", "Netherlands", "Chile", "Australia", "Spain", "Germany", "Ghana", "US", "Protugal"] )
-check("number_of_teams method", number_of_teams(team_list) == 12 )
-check("return_australia method", return_australia(team_list) == 'Australia' )
-check("starts_with_C method", starts_with_C(team_list) == ['Cameroon', 'Croatia', 'Chile'] )
-check("block_string_to_single_line method",
-block_string_to_single_line(long_string) == "Clint Dempsey put the U.S. ahead in the first minute of its World Cup opener." )
-check("capitalize_every_third_word method",
-capitalize_every_third_word(long_string) == "Clint dempsey put The u.s. ahead In the first Minute of its World cup opener." )
+  check("goal method", goal(goal_message) == "GOOOOOOOAL!!!!GOOOOOOOAL!!!!")
+  check("generate_an_array_of_teams method",
+  generate_an_array_of_teams(team_list) == ["Brazil", "Mexico", "Cameroon", "Croatia", "Netherlands", "Chile", "Australia", "Spain", "Germany", "Ghana", "US", "Protugal"] )
+  check("number_of_teams method", number_of_teams(team_list) == 12 )
+  check("return_australia method", return_australia(team_list) == 'Australia' )
+  check("starts_with_C method", starts_with_C(team_list) == ['Cameroon', 'Croatia', 'Chile'] )
+  check("block_string_to_single_line method",
+  block_string_to_single_line(long_string) == "Clint Dempsey put the U.S. ahead in the first minute of its World Cup opener." )
+  check("capitalize_every_third_word method",
+  capitalize_every_third_word(long_string) == "Clint dempsey put The u.s. ahead In the first Minute of its World cup opener." )
